ef0935b701d4a4b50ad686cde21349c37b49d2f8
USER-2U666ARN1Q\Administrator <3166832341@qq.com>
2023-12-31 22:11:55 +0800

    西武ATS初期開発準備

--- SeibuAts/SeibuATS.cs
+++ SeibuAts/SeibuATS.cs
@@ -15,8 +15,8 @@ namespace SeibuAts {
         public static IAtsPanelValue<bool> ATS_Power, ATS_EB, ATS_Limit, ATS_Stop, ATS_Confirm;
         public static IAtsSound ATS_StopAnnounce, ATS_EBAnnounce;
         private static bool Confirm = false, MaxOver95 = false;
-        private static double B1Point = 0, B2Point = 0;
-        private static int EBType = 0;
+        private static double PointLocation = 0, SignalLocation = 0;
+        private static int EBType = 0, PointType = 0, PatternType = 0; //4 -> G2,3 -> G1/YG,2 -> Y,1 -> YY,0 -> R
         public static int BrakeCommand = 0;
 
         public static void Initialize(AtsEx.PluginHost.Native.StartedEventArgs e) {
@@ -26,9 +26,12 @@ namespace SeibuAts {
             LimitPattern = new SpeedLimit();
 
             Confirm = MaxOver95 = false;
-            B1Point = B2Point = 0;
+            PointLocation = 0;
             BrakeCommand = 0;
             EBType = 0;
+            PointType = 0;
+            SignalLocation = 0;
+            PatternType = 0;
         }
 
         public static void Load() {
@@ -41,15 +44,18 @@ namespace SeibuAts {
             ATS_StopAnnounce = Native.AtsSounds.Register(8);
             ATS_EBAnnounce = Native.AtsSounds.Register(9);
 
-            ATSPattern = new SpeedLimit(); 
-            SignalPattern = new SpeedLimit(); 
+            ATSPattern = new SpeedLimit();
+            SignalPattern = new SpeedLimit();
             StopPattern = new SpeedLimit(); 
             LimitPattern = new SpeedLimit();
 
             Confirm = MaxOver95 = false;
-            B1Point = B2Point = 0;
+            PointLocation = 0;
             BrakeCommand = 0;
             EBType = 0;
+            PointType = 0;
+            SignalLocation = 0;
+            PatternType = 0;
         }
 
         public static void DoorOpened(AtsEx.PluginHost.Native.DoorEventArgs e) {
@@ -59,10 +65,10 @@ namespace SeibuAts {
         public static void BeaconPassed(AtsEx.PluginHost.Native.BeaconPassedEventArgs e) {
             switch (e.Type) {//1 2 5 8 20
                 case 1:
-                    B1Point = SeibuAts.state.Location;
+                    PointUpdate(SeibuAts.state.Location, 1, e.Distance);
                     break;
                 case 2:
-                    B2Point = SeibuAts.state.Location;
+                    PointUpdate(SeibuAts.state.Location, 2, e.Distance);
                     break;
                 case 5:
                     if (StopPattern == SpeedLimit.inf) StopPattern = new SpeedLimit(0, SeibuAts.state.Location + 590);
@@ -82,10 +88,59 @@ namespace SeibuAts {
 
         public static void Tick(double Location, double Speed, Section nextSection) {
 
+            if (PointType == 1) {
+                if (nextSection.CurrentSignalIndex == 0) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 0);
+                } else if (nextSection.CurrentSignalIndex == 1) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 30);
+                } else if (nextSection.CurrentSignalIndex == 2) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 65);
+                } else if (nextSection.CurrentSignalIndex == 3) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 95);
+                } else if (nextSection.CurrentSignalIndex == 4) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, MaxOver95 ? 115 : 95);
+                }
+            } else if (PointType == 2) {
+                if (nextSection.CurrentSignalIndex == 0) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 0);
+                } else if (nextSection.CurrentSignalIndex == 1) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 30);
+                } else if (nextSection.CurrentSignalIndex == 2) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 65);
+                } else if (nextSection.CurrentSignalIndex == 3) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, 95);
+                } else if (nextSection.CurrentSignalIndex == 4) {
+                    SignalPattern = new SpeedLimit(PointLocation + 200, MaxOver95 ? 115 : 95);
+                }
+            }
+
+            if (Location > SignalLocation && PointType != 0) { 
+                PointType = 0;
+                if (SignalPattern.Limit == 0) PatternType = 0;
+                else if (SignalPattern.Limit == 30) PatternType = 1;
+                else if (SignalPattern.Limit == 65) PatternType = 2;
+                else if (SignalPattern.Limit == 95) PatternType = 3;
+                else if (SignalPattern.Limit == 115) PatternType = 4;
+            }
+        }
+
+        private static void PointUpdate(double Location,int Type,double SigLocation) { // 1 ->B1 2 -> B2
+            PointType = Type;
+            PointLocation = Location;
+            SignalLocation = SigLocation + Location;
+            if (SignalPattern.Limit == 0) PatternType = 0;
+            else if (SignalPattern.Limit == 30) PatternType = 1;
+            else if (SignalPattern.Limit == 65) PatternType = 2;
+            else if (SignalPattern.Limit == 95) PatternType = 3;
+            else if (SignalPattern.Limit == 115) PatternType = 4;
         }
 
         public static void Dispose() {
-
+            ATS_Power.Dispose();
+            ATS_EB.Dispose();
+            ATS_Limit.Dispose();
+            ATS_Stop.Dispose();
+            ATS_Confirm.Dispose();
         }
     }
 }
