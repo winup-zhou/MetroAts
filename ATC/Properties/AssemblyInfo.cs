a8f7c62cee4c1dfab90dd71a4fa110fadeced2e6
USER-2U666ARN1Q\Administrator <3166832341@qq.com>
2024-01-07 12:57:26 +0800

    Update version number

--- ATC/Properties/AssemblyInfo.cs
+++ ATC/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ATC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ATCプラグイン for BVE TrainSim")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MS")]
+[assembly: AssemblyCompany("Win_Update Studio")]
 [assembly: AssemblyProduct("ATC")]
-[assembly: AssemblyCopyright("Copyright © MS 2023")]
+[assembly: AssemblyCopyright("Copyright © Win_Update Studio 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
